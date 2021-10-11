79decd7735b22853c44fe2262b250851067fbf87
Pragya Gupta <pragya18nsit@gmail.com>
2021-10-11 09:44:46 +0100

    character copier stage with mocks

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,34 +1,18 @@
 import { Copier } from "./copier";
 import { Destination } from "./destination";
 import { Source } from "./source";
+import { mock } from "jest-mock-extended";
 
-class SourceStub implements Source{
-  characters:string[] = ["a", "b", "c", "d", "e", "f","\n"];
-  index:number = 0;
-  getCharacter(): string {
-    const character = this.characters[this.index];
-    this.index=this.index+1;
-    return character;
-  }
-
-
-}
-
-class DestinationFake implements Destination{
-  characters:string[] = [];
-  setCharacter(character: string): void {
-    this.characters.push(character);
-  }
-}
 
 describe("Test of character copier", function() {
   it("reads characters from a source and copies them to a destination one character at a time.", function() {
-    const source = new SourceStub();
-    const destination = new DestinationFake();
+    // HOW SHOULD I SET UP SOURCE SO THAT IT RETURNS "a"
+    const source = mock<Source>();
+    const destination = mock<Destination>();
     const copier = new Copier(source, destination);
 
     copier.copy();
-    expect(destination.characters).toContainEqual("a");
+    expect(destination.setCharacter).toHaveBeenCalledWith("a");
   });
 
   it(" should read characters from the source and copy them to the destination until the source returns a newline.", function(){
